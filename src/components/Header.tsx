36a09abc58b6b2055a8c9e608e4a14cb963df328
404EZRA <77070161+404EZRA@users.noreply.github.com>
2024-09-04 18:00:06 -0700

    Added api key input

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import { BaseSyntheticEvent, useState } from "react";
-import { Settings } from "@mui/icons-material";
+import { Settings, Visibility, VisibilityOff } from "@mui/icons-material";
 import './Header.css';
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [input, setInput] = useState('');
+    const [isVisible, setIsVisible] = useState(true);
 
     const handleClick = () => {
         setIsOpen(!isOpen);
@@ -13,25 +14,37 @@ const Header = () => {
     const handleInputChange = (e: BaseSyntheticEvent) => {
         setInput(e.target.value);
     };
+
+    const handleVisibility = () => {
+        setIsVisible(!isVisible);
+    }
         
     return (
         <header>
-            <nav className='accordion'>
+            <nav className='header'>
                 <div className="accordian-header">
                     <Settings
-                        className={`icon ${isOpen ? 'open' : ''}`} 
+                        className={`settings-icon ${isOpen ? 'open' : ''}`} 
                         onClick={handleClick}
                         fontSize="large"
                     />
                 </div>
                 {isOpen && (
                 <div className="accordion-content">
-                    <input
-                        className='api-key-input'
-                        value={input}
-                        onChange={handleInputChange}
-                        placeholder="OpenAI API Key"
-                    />
+                    <h4>Settings</h4>
+                    <div className="api-key-component">
+                        <input
+                            type={isVisible ? 'text' : 'password'}
+                            className='api-key-input'
+                            value={input}
+                            onChange={handleInputChange}
+                            placeholder="OpenAI API Key*"
+                        />
+                        <div className="visibility-button">
+                            {isVisible ? <VisibilityOff onClick={handleVisibility} /> : <Visibility onClick={handleVisibility} />}
+                        </div>
+                    </div>
+                    <span className="disclaimer">*ChatGPT API Key is required in order for this application to function properly.</span>
                 </div>
             )}
             </nav>
